49ae7f1a4ee93208e032685538c0f31412307baa
yoelliem03 <144304026+yoelliem03@users.noreply.github.com>
2024-04-25 02:38:09 +0700

    Initial architecture

---
+++ README.md
@@ -0,0 +1,16 @@
+# Facial-Recognition-Auth 🚀
+
+> **Highly accurate biometric authentication service using deep neural networks.**
+
+![License](https://img.shields.io/badge/license-Apache--2.0-blue.svg)
+![Build Status](https://img.shields.io/badge/build-passing-brightgreen.svg)
+![Architecture](https://img.shields.io/badge/Architecture-Microservices-orange)
+
+## Overview
+Highly accurate biometric authentication service using deep neural networks. 
+Built with scalability, low-latency, and high availability utilizing advanced Go architecture.
+
+## Deployment
+Use the included Kubernetes manifests to deploy to your cluster.
+
+*Property of yoelliem03.*
---
+++ backend-go/server.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "enterprise/api/v1"
+)
+
+type GrpcServer struct {
+	pb.UnimplementedEnterpriseServiceServer
+	mu sync.RWMutex
+	activeConnections int
+}
+
+func (s *GrpcServer) ProcessStream(stream pb.EnterpriseService_ProcessStreamServer) error {
+	ctx := stream.Context()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Client disconnected")
+			return ctx.Err()
+		default:
+			req, err := stream.Recv()
+			if err != nil { return err }
+			go s.handleAsync(req)
+		}
+	}
+}
+
+func (s *GrpcServer) handleAsync(req *pb.Request) {
+	s.mu.Lock()
+	s.activeConnections++
+	s.mu.Unlock()
+	time.Sleep(10 * time.Millisecond) // Simulated latency
+	s.mu.Lock()
+	s.activeConnections--
+	s.mu.Unlock()
+}
